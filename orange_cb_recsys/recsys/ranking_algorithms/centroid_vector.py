20c2963e0609acb0f8a688e9cc5dd98d30752927
Elio <elio.musacchio28@gmail.com>
2021-03-28 23:00:34 +0200

    Miglioramenti implementazione centroid vector

--- orange_cb_recsys/recsys/ranking_algorithms/centroid_vector.py
+++ orange_cb_recsys/recsys/ranking_algorithms/centroid_vector.py
@@ -1,80 +1,37 @@
 from typing import List
 
+from sklearn.feature_extraction import DictVectorizer
+
 from orange_cb_recsys.content_analyzer.content_representation.content_field import EmbeddingField, FeaturesBagField
-from orange_cb_recsys.recsys.algorithm import ItemFieldRankingAlgorithm, Transformer
-from orange_cb_recsys.recsys.ranking_algorithms.similarities import Similarity, DenseVector, CosineSimilarity
+from orange_cb_recsys.recsys.ranking_algorithms.item_fields_algorithm import\
+    ItemFieldsRankingAlgorithm, transform
+from orange_cb_recsys.recsys.ranking_algorithms.similarities import Similarity, CosineSimilarity
 import pandas as pd
 import numpy as np
 
 from orange_cb_recsys.utils.const import logger
 
 
-class CentroidVector(Transformer):
-    """
-    Class used to compute the centroid of positive rated items by a user and
-    the similarity between the centroid and the items not rated by said user.
-
-    Args:
-        similarity (Similarity): similarity implementation to compute
-    """
-
-    def __init__(self, similarity: Similarity):
-        super().__init__()
-        self.__centroid = None
-        self.__similarity = similarity
-
-    def compute_centroid(self, positive_rated_items: list):
-        """
-        Transforms the positive rated item fields representations into a single one, using the
-        method transform of the Transformer class, and computes the centroid of the items positively rated by a user.
-
-        Args:
-            positive_rated_items (list): list of positive item representations for each field
-        """
-        positive_rated_items = super().transform(positive_rated_items)
-
-        self.__centroid = np.array(positive_rated_items).mean(axis=0)
-
-    def compute_similarities(self, unrated_items: list):
-        """
-        Transforms the unrated item fields representations, using the method transform of the Transformer class, and
-        computes the similarity between the centroid and the unrated items.
-
-        Args:
-            unrated_items (list): list of unrated item representations for each field
-
-        Returns:
-            similarity (list): list of similarity scores between each unrated item and the positive rated items centroid
-        """
-        unrated_items = super().transform(unrated_items)
-
-        dense_centroid = DenseVector(self.__centroid)
-        similarity = [self.__similarity.perform(dense_centroid, DenseVector(item)) for item in unrated_items]
-
-        return similarity
-
-
-class CentroidVectorRecommender(ItemFieldRankingAlgorithm):
+class CentroidVectorRecommender(ItemFieldsRankingAlgorithm):
     """
     Class that implements a centroid-like recommender. It first gets the centroid of the items that the user liked and
-    then computes the similarity between the centroid and the item of which predict the score, using a CentroidVector
-    object instantiated inside the class. The similarity can be chosen and passed as an argument, otherwise
-    CosineSimilarity is adopted by default.
+    then computes the similarity between the centroid and each item of which predict the score.
+    The similarity can be chosen and passed as an argument, otherwise CosineSimilarity is adopted by default.
         EXAMPLE:
             Sets the dictionary containing only one field (Plot) with one representation, CosineSimilarity as similarity
             measure and the threshold as 1
                 CentroidVectorRecommender(item_field={'Plot' : '1'}, similarity=CosineSimilarity(), threshold=1)
 
     Args:
-        item_field (dict): dictionary containing the field names and representations to consider of the items
-        similarity (Similarity): kind of similarity to use
+        item_fields (dict): dictionary containing the field names and representations to consider of the items
+        similarity (Similarity): kind of similarity to use, object of the Similarity class
         threshold (int): threshold for the ratings. If the rating is greater than the threshold, it will be considered
             as positive
     """
 
-    def __init__(self, item_field: dict, similarity: Similarity = CosineSimilarity(), threshold: int = -1):
-        super().__init__(item_field, threshold)
-        self.__centroid_vector = CentroidVector(similarity)
+    def __init__(self, item_fields: dict, similarity: Similarity = CosineSimilarity(), threshold: int = -1):
+        super().__init__(item_fields, threshold)
+        self.__similarity = similarity
 
     def __calc_positive_rated_baglist(self, rated_items: list, ratings: pd.DataFrame):
         """
@@ -95,11 +52,10 @@ class CentroidVectorRecommender(ItemFieldRankingAlgorithm):
         """
         positive_features_bag_list = []
         for item in rated_items:
-            single_item_bag_list = []
-
-            if item is not None:
-                for item_field in self.item_field:
-                    field_representations = self.item_field[item_field]
+            if item is not None and float(ratings[ratings['to_id'] == item.content_id].score) >= self.threshold:
+                single_item_bag_list = []
+                for item_field in self.item_fields:
+                    field_representations = self.item_fields[item_field]
 
                     if isinstance(field_representations, str):
                         # We have only one representation
@@ -119,8 +75,7 @@ class CentroidVectorRecommender(ItemFieldRankingAlgorithm):
 
                             single_item_bag_list.append(item_representation.value)
 
-                if float(ratings[ratings['to_id'] == item.content_id].score) >= self.threshold:
-                    positive_features_bag_list.append(single_item_bag_list)
+                positive_features_bag_list.append(single_item_bag_list)
 
         if len(ratings) == 0:
             raise FileNotFoundError("No rated items available locally!\n"
@@ -134,7 +89,7 @@ class CentroidVectorRecommender(ItemFieldRankingAlgorithm):
     @staticmethod
     def __check_representation(representation, representation_name: str, item_field: str):
         """
-        Checks that the passed representations is an embedding (in which case the granularity must be document)
+        Checks that the passed representation is an embedding (in which case the granularity must be document)
         or a tf-idf vector, otherwise throws an exception because in these scenarios the centroid calculation
         cannot be computed
 
@@ -155,24 +110,24 @@ class CentroidVectorRecommender(ItemFieldRankingAlgorithm):
                     "The representation %s for the %s field is not a document embedding, "
                     "so the centroid cannot be calculated" % (representation_name, item_field))
 
-    def predict(self, user_id: str, ratings: pd.DataFrame, recs_number: int, items_directory: str,
+    def predict(self, ratings: pd.DataFrame, recs_number: int, items_directory: str,
                 candidate_item_id_list: List = None) -> pd.DataFrame:
         """
         After computing the centroid of the positive rated items by the user and getting the similarity scores
-        of said centroid compared with every unrated item (using the CentroidVector object), creates and
-        returns a recommendation list of unrated items ordered by their similarity score with the centroid
+        of said centroid compared with every unrated item, creates and returns a recommendation list of unrated
+        items ordered by their similarity score with the centroid. A candidate_item_id_list can be passed
+        which will be used instead of the unrated items.
 
             EXAMPLE:
                 Creates a recommendation list of length 1 with the similarity to the centroid as score, only considering
                 the item tt0114319 instead of all the unrated items. (Ratings is a DataFrame containing the ratings
-                given by the user with id A000)
-                    predict(user_id='A000', ratings=ratings, recs_number=1, items_directory='.../somedir',
+                given by the user)
+                    predict(ratings=ratings, recs_number=1, items_directory='.../somedir',
                     candidate_item_id_list=['tt0114319'])
 
         Args:
             candidate_item_id_list (list): list of the items that can be recommended, if None
                 all unrated items will be used
-            user_id (str): user for which compute the prediction
             recs_number (int): how long the ranking will be
             ratings (pd.DataFrame): ratings of a user
             items_directory (str): name of the directory where the items are stored.
@@ -181,7 +136,7 @@ class CentroidVectorRecommender(ItemFieldRankingAlgorithm):
              scores (pd.DataFrame): DataFrame whose columns are the ids of the items (to_id),
                 and the similarities between the items and the centroid (rating)
         """
-
+        transformer = DictVectorizer(sparse=True, sort=False)
         # Loads the items and extracts features from the unrated items, then
         # extracts features from the positive rated items
         # If exception, returns an empty score_frame
@@ -196,19 +151,21 @@ class CentroidVectorRecommender(ItemFieldRankingAlgorithm):
             return score_frame
 
         logger.info("Computing rated items centroid")
-        self.__centroid_vector.compute_centroid(positive_rated_features_bag_list)
+        positive_rated_items_array = transform(transformer, positive_rated_features_bag_list)
+        centroid = np.array(positive_rated_items_array).mean(axis=0)
 
         columns = ["to_id", "rating"]
         score_frame = pd.DataFrame(columns=columns)
 
         logger.info("Computing similarity between centroid and unrated items")
-        similarities = self.__centroid_vector.compute_similarities(unrated_features_bag_list)
+        unrated_items_array = transform(transformer, unrated_features_bag_list)
+        similarities = [self.__similarity.perform(centroid, item) for item in unrated_items_array]
+
         for item, similarity in zip(unrated_items, similarities):
-            if item is not None:
-                score_frame = pd.concat(
-                    [score_frame,
-                     pd.DataFrame.from_records([(item.content_id, similarity)], columns=columns)],
-                    ignore_index=True)
+            score_frame = pd.concat(
+                [score_frame,
+                 pd.DataFrame.from_records([(item.content_id, similarity)], columns=columns)],
+                ignore_index=True)
 
         score_frame = score_frame.sort_values(['rating'], ascending=False).reset_index(drop=True)
         score_frame = score_frame[:recs_number]
--- orange_cb_recsys/recsys/ranking_algorithms/similarities.py
+++ orange_cb_recsys/recsys/ranking_algorithms/similarities.py
@@ -1,9 +1,17 @@
 from abc import ABC, abstractmethod
 from scipy import spatial
-from sklearn.metrics.pairwise import cosine_similarity
+
+import numpy as np
 
 
 class Vector(ABC):
+    """
+    Abstract class used to represent a vector. This is useful for similarity computation
+    which can be customized thanks to the similarity abstract method.
+
+    Args:
+        value: value to store inside of the Vector (a numpy array is used in our scenarios)
+    """
     def __init__(self, value):
         self.__value = value
 
@@ -18,25 +26,32 @@ class Vector(ABC):
 
 class DenseVector(Vector):
     def similarity(self, other):
+        """
+        Computes cosine similarity between the stored value and another value passed as argument
+
+        Args:
+            other: value to compare with the value saved in the object attributes (a numpy array
+                is used in our scenarios)
+        """
         return 1 - spatial.distance.cosine(self.value, other.value)
-
-
-class SparseVector(Vector):
-    def similarity(self, other):
-        return cosine_similarity(self.value, other.value)
 
 
 class Similarity(ABC):
     """
-    Class for the various types of similarity
+    Abstract class for the various types of similarity, these types of similarity
+    can be used, for example, in the CentroidVectorRecommender
     """
     def __init__(self):
         pass
 
     @abstractmethod
-    def perform(self, v1: Vector, v2: Vector):
+    def perform(self, v1: np.ndarray, v2: np.ndarray):
         """
         Calculates the similarity between v1 and v2
+
+        Args:
+            v1(np.ndarray): numpy array
+            v2(np.ndarray): numpy array
         """
         raise NotImplementedError
 
@@ -48,5 +63,14 @@ class CosineSimilarity(Similarity):
     def __init__(self):
         super().__init__()
 
-    def perform(self, v1: Vector, v2: Vector):
-        return v1.similarity(v2)
+    def perform(self, v1: np.ndarray, v2: np.ndarray):
+        """
+        Calculates the similarity between two numpy arrays.
+        Instantiates two Vector objects and passes one of the two arrays to each one of them.
+        Then the similarity is computed using the similarity method of the Vector object.
+
+        Args:
+            v1(np.ndarray): numpy array
+            v2(np.ndarray): numpy array
+        """
+        return DenseVector(v1).similarity(DenseVector(v2))
