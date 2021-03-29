4410310b32ef1d1d59a484c11ab431696c6d30c7
Roberta Sallustio <61421614+Yukiroby@users.noreply.github.com>
2021-03-30 00:23:23 +0200

    Modifiche al testing legate all'embedding_learner

--- test/content_analyzer/embedding_learner/test_random_indexing.py
+++ test/content_analyzer/embedding_learner/test_random_indexing.py
@@ -1,17 +1,18 @@
+import os
 from unittest import TestCase
+
+import gensim
 
 from orange_cb_recsys.content_analyzer.embedding_learner.random_indexing import GensimRandomIndexing
 from orange_cb_recsys.content_analyzer.information_processor.nlp import NLTK
 from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class TestRandomIndexing(TestCase):
     def test_fit(self):
-        file_path = '../../../datasets/movies_info_reduced.json'
-        try:
-            with open(file_path):
-                pass
-        except FileNotFoundError:
-            file_path = 'datasets/movies_info_reduced.json'
-
-        GensimRandomIndexing(JSONFile(file_path), NLTK(), ['Genre', 'Plot']).fit()
+        file_path = os.path.join(THIS_DIR, "../../../datasets/movies_info_reduced.json")
+        random_indexing = GensimRandomIndexing(JSONFile(file_path), NLTK(), ['Genre', 'Plot'])
+        random_indexing.fit()
+        self.assertIsInstance(random_indexing.model, gensim.models.rpmodel.RpModel)
