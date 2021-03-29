038771567922f5a0f43763af65ec2ee1a6e075c4
Roberta Sallustio <61421614+Yukiroby@users.noreply.github.com>
2021-03-30 00:27:15 +0200

    Modifiche al testing legate all'evaluation

--- test/evaluation/test_check_tokenization.py
+++ test/evaluation/test_check_tokenization.py
@@ -6,7 +6,11 @@ class Test(TestCase):
     def test_check_tokenized(self):
         str_ = 'abcd efg'
         list_ = ['abcd', 'efg']
-        check_tokenized(str_)
-        check_tokenized(list_)
-        check_not_tokenized(str_)
-        check_not_tokenized(list_)
+        x = check_tokenized(str_)
+        self.assertEqual(x, ['abcd', 'efg'])
+        y = check_tokenized(list_)
+        self.assertEqual(y, ['abcd', 'efg'])
+        z = check_not_tokenized(str_)
+        self.assertEqual(z, 'abcd efg')
+        s = check_not_tokenized(list_)
+        self.assertEqual(s, 'abcd efg')
--- test/evaluation/test_delta_gap.py
+++ test/evaluation/test_delta_gap.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
-from orange_cb_recsys.evaluation.delta_gap import calculate_gap
+from orange_cb_recsys.evaluation.delta_gap import calculate_gap, calculate_delta_gap
 
 
 class Test(TestCase):
     def test_calculate_gap(self):
         group = {'aaa', 'bbb', 'ccc'}
         avg_pop = {'aaa': 0.5, 'bbb': 0.7}
-        calculate_gap(group, avg_pop)
+        gap = calculate_gap(group, avg_pop)
+        self.assertEqual(round(gap, ndigits=2), 0.4)
+
+    def test_calculate_delta_gap(self):
+        delta_gap = calculate_delta_gap(1.0, 0.0)
+        self.assertEqual(delta_gap, 0.0)
 
