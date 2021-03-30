442efa213d4d99fdf90564b3bbc77df268a93abe
Roberta Sallustio <61421614+Yukiroby@users.noreply.github.com>
2021-03-30 15:59:33 +0200

    Rimozione uso di glob

--- test/content_analyzer/test_content_analyzer_main.py
+++ test/content_analyzer/test_content_analyzer_main.py
@@ -1,4 +1,3 @@
-import glob
 import os
 from unittest import TestCase
 import lzma
@@ -16,23 +15,22 @@ from orange_cb_recsys.content_analyzer.field_content_production_techniques.tf_id
 from orange_cb_recsys.content_analyzer.information_processor import NLTK
 from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
 
-filepath = '../../datasets/movies_info_reduced.json'
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(THIS_DIR, "../../datasets/movies_info_reduced.json")
 
 
 class TestContentsProducer(TestCase):
     def test_create_content(self):
-        file_path1 = os.path.join(THIS_DIR, "../../datasets/movies_info_reduced.json")
-        file_path2 = os.path.join(THIS_DIR, "../../test/content_analyzer/movielens_test*")
+        file_path_content_analyzer = os.path.join(THIS_DIR, "../../test/content_analyzer/movielens_test*")
         entity_linking_pipeline = FieldRepresentationPipeline(BabelPyEntityLinking())
         plot_config = FieldConfig(None)
         plot_config.append_pipeline(entity_linking_pipeline)
-        content_analyzer_config = ContentAnalyzerConfig('ITEM', JSONFile(file_path1), ["imdbID"], "movielens_test")
+        content_analyzer_config = ContentAnalyzerConfig('ITEM', JSONFile(file_path), ["imdbID"], "movielens_test")
         content_analyzer_config.append_field_config("Plot", plot_config)
         content_analyzer = ContentAnalyzer(content_analyzer_config)
         content_analyzer.fit()
-
-        glob_path = (glob.glob(file_path2)[0])
+        """
+        glob_path = (glob.glob(file_path_content_analyzer)[0])
         content_list = []
         for filename in os.listdir(glob_path):
             if filename.endswith(".xz"):
@@ -42,11 +40,12 @@ class TestContentsProducer(TestCase):
         self.assertIn("tt0112281", content_list[0].content_id)
         representation = content_list[0].field_dict["Plot"].get_representation("0")
         self.assertIsInstance(representation, FeaturesBagField)
+        """
 
     def test_create_content_tfidf(self):
         movies_ca_config = ContentAnalyzerConfig(
             content_type='Item',
-            source=JSONFile(filepath),
+            source=JSONFile(file_path),
             id_field_name_list=['imdbID'],
             output_directory="movielens_test",
         )
@@ -64,7 +63,7 @@ class TestContentsProducer(TestCase):
     def test_create_content_embedding(self):
         movies_ca_config = ContentAnalyzerConfig(
             content_type='Item',
-            source=JSONFile(filepath),
+            source=JSONFile(file_path),
             id_field_name_list=['imdbID'],
             output_directory="movielens_test",
         )
@@ -87,12 +86,12 @@ class TestContentsProducer(TestCase):
         content_analyzer.fit()
 
     def test_decode_field_data_string(self):
-        file_path = os.path.join(THIS_DIR, "../../datasets/test_decode/movies_title_string.json")
+        file_path_test_decode = os.path.join(THIS_DIR, "../../datasets/test_decode/movies_title_string.json")
         test_dir = os.path.join(THIS_DIR, "../../datasets/test_decode/")
 
         movies_ca_config = ContentAnalyzerConfig(
             content_type='Item',
-            source=JSONFile(file_path),
+            source=JSONFile(file_path_test_decode),
             id_field_name_list=['imdbID'],
             output_directory=test_dir + 'movies_string_'
         )
@@ -120,12 +119,12 @@ class TestContentsProducer(TestCase):
                     break
 
     def test_decode_field_data_tfidf(self):
-        file_path = os.path.join(THIS_DIR, "../../datasets/test_decode/movies_title_tfidf.json")
+        file_path_test_decode = os.path.join(THIS_DIR, "../../datasets/test_decode/movies_title_tfidf.json")
         test_dir = os.path.join(THIS_DIR, "../../datasets/test_decode/")
 
         movies_ca_config = ContentAnalyzerConfig(
             content_type='Item',
-            source=JSONFile(file_path),
+            source=JSONFile(file_path_test_decode),
             id_field_name_list=['imdbID'],
             output_directory=test_dir + 'movies_tfidf_'
         )
@@ -152,12 +151,12 @@ class TestContentsProducer(TestCase):
                     break
 
     def test_decode_field_data_embedding(self):
-        file_path = os.path.join(THIS_DIR, "../../datasets/test_decode/movies_title_embedding.json")
+        file_path_test_decode = os.path.join(THIS_DIR, "../../datasets/test_decode/movies_title_embedding.json")
         test_dir = os.path.join(THIS_DIR, "../../datasets/test_decode/")
 
         movies_ca_config = ContentAnalyzerConfig(
             content_type='Item',
-            source=JSONFile(file_path),
+            source=JSONFile(file_path_test_decode),
             id_field_name_list=['imdbID'],
             output_directory=test_dir + 'movies_embedding_'
         )
